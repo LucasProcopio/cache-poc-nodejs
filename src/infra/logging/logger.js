c7145e20a1f88c8cbf2520f8d311ee77cc935a5e
João Paulo <joao.silveira@4all.com>
2020-10-15 14:10:27 -0300

    logger improvement

--- src/infra/logging/logger.js
+++ src/infra/logging/logger.js
@@ -1,21 +1,44 @@
 const winston = require('winston');
+
 /**
-    * @param {Object} container - Dependency Injection.
-    * @param {import('../../../config/index')} container.config
-    */
+ * @param {Object} container - Dependency Injection.
+ * @param {import('../../../config/index')} container.config
+ */
 module.exports = ({ config }) => {
+    const defaultMessageFormat = winston.format.printf(msg => {
+        const { level, timestamp, message, stack } = msg;
+
+        let log = typeof message === 'object' ?
+            JSON.stringify(msg.message) :
+            message || msg[Symbol.for('message')];
+
+        if (stack) {
+            log = stack;
+        }
+
+        return `${timestamp} / ${level}: ${log}`;
+    });
+
+    const appFormat = winston.format.combine(
+        winston.format.errors({ stack: true }),
+        winston.format.timestamp(),
+        defaultMessageFormat,
+    );
+
     const options = {
         levels: winston.config.syslog.levels,
-        format: winston.format.json(),
+        format: winston.format.combine(
+            appFormat,
+            winston.format.json(),
+        ),
         defaultMeta: { service: config.application.service_name },
         transports: [
-            new winston.transports.Console({ format: winston.format.combine(
-                winston.format.timestamp(),
-                winston.format.colorize(),
-                winston.format.printf(msg => {
-                    const message = typeof msg.message === 'object' ? JSON.stringify(msg.message) : msg.message || msg[Symbol.for('message')];
-                    return `${msg.timestamp} / ${msg.level}: ${message}`;
-                })) }),
+            new winston.transports.Console({
+                format: winston.format.combine(
+                    winston.format.colorize(),
+                    appFormat,
+                ),
+            }),
             new winston.transports.File({ filename: config.application.log_path })
         ]
     };
@@ -23,7 +46,7 @@ module.exports = ({ config }) => {
     const logger = winston.createLogger(options);
 
     logger.stream = {
-        write: function(message, encoding){
+        write: function(message, encoding) {
             logger.info(message.substring(0, message.lastIndexOf('\n')));
         }
     };
